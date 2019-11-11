761fe2f09b0a4ab3bb8ad65e2872c4ec9c7fc5a5
mbj <924285942@qq.com>
2019-11-11 23:23:38 +0800

    Important update on code formatting and comments, make it easier to understand

--- clock.js
+++ clock.js
@@ -1,57 +1,58 @@
-var minute,second;//分 秒
-minute=second=0;//初始化
-var STOP=0;
+var minute, second; //分 秒
+minute = second = 0; //初始化
+var STOP = 0;
 
 var int;
-function Reset()//重置
+
+function Reset() //重置
 {
-  window.clearInterval(int);
-  minute=second=0;
-  document.getElementById('timetext').innerHTML='00:00';
+    window.clearInterval(int);
+    minute = second = 0;
+    document.getElementById('timetext').innerHTML = '00:00';
 }
 
-function start()//开始
+function start() //开始
 {
-  int=setInterval(timer,1000);
+    int = setInterval(timer, 1000);
 }
 //暂停秒表，attention:变量answering和STOP
-function stop(){
-  if(answering){
-    alert("Your are viewing answer, CAN'T PAUSE");
-    return ;
-  }
-  if(STOP==0){
-    clearInterval(int);
-    STOP=1;
-  }else{
-    int=setInterval(timer,1000);
-    STOP=0;
-  }
+function stop() {
+    if (answering) {
+        alert("Your are viewing answer, CAN'T PAUSE");
+        return;
+    }
+    if (STOP == 0) {
+        clearInterval(int);
+        STOP = 1;
+    } else {
+        int = setInterval(timer, 1000);
+        STOP = 0;
+    }
 }
 
 //计时
-function timer(){
+function timer() {
     second++;
-  //60秒后清0
-  if(second>=60){
-    second=0;
-    minute=minute+1;
-  }
+    //60秒后清0
+    if (second >= 60) {
+        second = 0;
+        minute = minute + 1;
+    }
 
-  if(minute>=60){
-    alert("Too much time!");
-    Reset();
-    start();
-  }
-  document.getElementById('timetext').innerHTML=printf(minute)+':'+printf(second);
+    if (minute >= 60) {
+        alert("Too much time!");
+        Reset();
+        start();
+    }
+    document.getElementById('timetext').innerHTML = printf(minute) + ':' + printf(second);
 
 }
 //字符串转换函数
-function printf(t){
-  var s=""+t;
-  if(s.length==1){
-    return "0"+s;
-  }else{
-    return s;
-  }
+function printf(t) {
+    var s = "" + t;
+    if (s.length == 1) {
+        return "0" + s;
+    } else {
+        return s;
+    }
 }
--- judge.js
+++ judge.js
@@ -1,40 +1,41 @@
 //检查如果(row,col)放入c，是否符合规则
-function isValid(board, row, col, c){
-    if(board[row][col] == c) return true;
-    for(var i = 0; i < 9; i++) {
-        if(board[i][col] != "" && board[i][col] == c) return false; //check row
-        if(board[row][i] != "" && board[row][i] == c) return false; //check column
-        if(board[3 * Math.floor(row / 3) + Math.floor(i / 3)][ 3 * Math.floor(col / 3) + Math.floor(i % 3)] != "" && 
-board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] == c) return false; //check 3*3 block
+function isValid(board, row, col, c) {
+    if (board[row][col] == c) return true;
+    for (var i = 0; i < 9; i++) {
+        if (board[i][col] != "" && board[i][col] == c) return false; //check row
+        if (board[row][i] != "" && board[row][i] == c) return false; //check column
+        if (board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] != "" &&
+            board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] == c) return false; //check 3*3 block
     }
     return true;
 }
 //检查最终结果
-function checkResult(){
+function checkResult() {
     //假如已经查看答案，这个按钮不能按(后期变灰)
-    if(answering){
-      alert("You are viewing answer!");
-      return;
+    if (answering) {
+        alert("You are viewing answer!");
+        return;
     }
-    for (var i = 0; i < 9; i++){
-      for (var j = 0; j < 9; j++){
-        var check = isFinished(board, i, j, board[i][j]);
-        if (check == false){
-          alert("Answer Wrong!");
-          return;
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            var check = isFinished(board, i, j, board[i][j]);
+            if (check == false) {
+                alert("Answer Wrong!");
+                return;
+            }
         }
-      }
     }
     alert("~Congratulations~")
-  }
-  function isFinished(board, row, col, c){
-    for(var i = 0; i < 9; i++) {
+}
+
+function isFinished(board, row, col, c) {
+    for (var i = 0; i < 9; i++) {
         if (board[row][col] == "") return false;
-        if(board[i][col] != "" && board[i][col] == c && i != row) return false; //check row
-        if(board[row][i] != "" && board[row][i] == c && i != col) return false; //check column
-        if(board[3 * Math.floor(row / 3) + Math.floor(i / 3)][ 3 * Math.floor(col / 3) + Math.floor(i % 3)] != "" && 
-  board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] == c && 
-  ((3 * Math.floor(row / 3) + Math.floor(i / 3)) != row && (3 * Math.floor(col / 3) + Math.floor(i % 3)) != col)) return false; //check 3*3 block
+        if (board[i][col] != "" && board[i][col] == c && i != row) return false; //check row
+        if (board[row][i] != "" && board[row][i] == c && i != col) return false; //check column
+        if (board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] != "" &&
+            board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + Math.floor(i % 3)] == c &&
+            ((3 * Math.floor(row / 3) + Math.floor(i / 3)) != row && (3 * Math.floor(col / 3) + Math.floor(i % 3)) != col)) return false; //check 3*3 block
     }
     return true;
-  }
+}
