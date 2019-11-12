acef291974c89501181f8aacd62cc583201b34f8
mbj <924285942@qq.com>
2019-11-12 21:00:57 +0800

    Add buttons, update layout and color and link on title

--- generateSudoku.js
+++ generateSudoku.js
@@ -62,6 +62,7 @@ function getRandomNum() {
 }
 
 //递归，根据第一行生成总的board
+//剪枝
 function solve(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
